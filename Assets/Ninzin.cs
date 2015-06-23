a5b01b1f1946ebf576beb87f8b56cf1b02409ea5
EleaNoR <vyorukazev@gmail.com>
2015-06-24 03:19:47 +0900

    eleanor

---
+++ Assets/Ninzin.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ninzin : MonoBehaviour{
+
+	// 爆発のPrefab
+	public GameObject explosion;
+	
+	// 爆発の作成
+	public void Explosion ()
+	{
+		Instantiate (explosion, transform.position, transform.rotation);
+	}
+
+	void OnTriggerEnter2D (Collider2D c)
+	{
+		// 弾の削除
+		Destroy(c.gameObject);
+
+		// プレイヤーを削除
+		Destroy (gameObject);
+	}
+}
